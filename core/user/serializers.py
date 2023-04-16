b4c65bba9744829c407e33e9cbc878f23658b672
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-04-16 18:59:39 -0400

    add swagger

---
+++ core/user/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from core.user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "name",
+            "first_name",
+            "last_name",
+            "email",
+            "is_active",
+            "created",
+            "updated",
+            "bio",
+            "avatar",
+        ]
+        read_only_fields = ['is_active']
