241bc2886785dd04ec5748fa447ef3db32ddf8a2
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-04-24 23:47:54 -0400

    added auth logic

--- core/routers.py
+++ core/routers.py
@@ -1,11 +1,17 @@
 from rest_framework import routers
 
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 from core.user.viewsets import UserViewSet
 
 router = routers.SimpleRouter()
 
 # USER
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet,
+                basename='auth-register')
+router.register(r'auth/login', LoginViewSet,
+                basename='auth-login')
+
 urlpatterns = [
     *router.urls,
 ]
