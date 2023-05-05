8dbf1b1980c211c6d98e6e99d62f20093fb3a7ab
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-05-05 09:01:52 -0400

    added put and delete on Post

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -10,6 +10,12 @@ class PostSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(
         queryset=User.objects.all(), slug_field="public_id")
 
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data["edited"] = True
+        instance = super().update(instance, validated_data)
+        return instance
+
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         author = User.objects.get_object_by_public_id(rep["author"])
