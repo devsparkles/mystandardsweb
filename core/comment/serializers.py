a82d5f18059eef215021b9c6f41824962028dd92
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-05-06 23:57:46 -0400

    can : add comment update comment

--- core/comment/serializers.py
+++ core/comment/serializers.py
@@ -19,6 +19,18 @@ class CommentSerializer(AbstractSerializer):
         rep["author"] = UserSerializer(author).data
         return rep
 
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+        instance = super().update(instance,
+                                  validated_data)
+        return instance
+
+    def validate_post(self, value):
+        if self.instance:
+            return self.instance.post
+        return value
+
     class Meta:
         model = Comment
         # List of all the fields that can be included in a
