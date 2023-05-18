ed6a660f2a147ac441478e43b562b5a2eca8b21d
Xavier Maximin <xavier.maximin.uk@gmail.com>
2023-05-17 22:18:04 -0400

    add login registration

--- social-media-app/src/routes/ProtectedRoutes.jsx
+++ social-media-app/src/routes/ProtectedRoutes.jsx
@@ -6,12 +6,8 @@ import { Navigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
 
     const { user } = JSON.parse(localStorage.getItem("auth"));
-      console.log("xavier:"+ user);
-   
     return user ? <>{children}</> : <Navigate
-   
       to="/login/" />;
-   
-   }
-   
-   export default ProtectedRoute;
+}
+
+export default ProtectedRoute;
